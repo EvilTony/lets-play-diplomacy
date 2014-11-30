55c7f994b6d25f5d8e561325dedfb0492f3ccb30
Taylor1791 <Taylor@Everding.email>
2014-11-30 10:12:07 -0600

    rough map

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 var files = require( './front-end-workflow/gulpfile' );
 
 files.node = [ 'gulpfile.js' ];
-files.json = [ 'package.json' ];
+files.browser = [ 'client/**/*.js' ];
+files.json = [ 'package.json', 'client/**/*.json' ];
+files.html = [ 'client/**/*.html' ];
 files.serve = [
-  'node_modules/normalize.css',
+  'client',
+  'node_modules/d3',
   'node_modules/less/dist',
-  'client'
+  'node_modules/normalize.css',
+  'node_modules/ramda'
 ];
