d5247a3967b35b9792e3f215e9f0bd99616178a7
Taylor1791 <Taylor@Everding.email>
2014-12-08 00:05:41 -0600

    adds very rough labels to map

--- client/app/components/map/render.js
+++ client/app/components/map/render.js
@@ -7,26 +7,53 @@ d3.json( '/app/components/map/map-data.json', function( err, rawBoard ) {
   var
     width = 550,
     height = 550,
+
     projection = cart2dProjection()
       .scale( R.repeatN( 26, 2 ) ),
     path = d3.geo.path()
       .projection( projection ),
+
     svg = d3.select( 'main' )
       .append( 'svg' )
       .attr( 'width', width )
       .attr( 'height', height ),
+    provs  = svg
+      .append( 'g' )
+      .attr( 'class', 'provinces' ),
+    labels = svg
+      .append( 'g' )
+      .attr( 'class', 'labels' ),
 
-    addAbbr = R.mapObj.idx( R.flip( R.assoc( 'abbr' ) ) ),
-    addPolygon = R.mapObj( R.assoc( 'type', 'Polygon' ) ),
-    polygonizeBoard = R.compose( R.values, addAbbr, addPolygon ),
+    labelX = R.compose( R.subtract( void( 0 ), 20 ), R.head, path.centroid ),
+    labelY = R.compose( R.add( 5 ), R.nth( 1 ), path.centroid ),
+    labelText = R.prop( 'abbr' ),
+
+    addAbbr = R.flip( R.assoc ( 'abbr' ) ),
+    addPolygonType = R.assoc( 'type', 'Polygon' ),
+
+    addAbbrs = R.mapObj.idx( addAbbr ),
+    mkPolygons = R.mapObj( addPolygonType ),
+
+    polygonizeBoard = R.compose( R.values, addAbbrs, mkPolygons ),
     polygonBoard = polygonizeBoard( rawBoard );
 
-  svg
+  provs
     .selectAll( 'path' )
     .data( polygonBoard )
     .enter()
       .append( 'path' )
-      .attr( 'd', path  )
-      .attr( 'class', R.prop( 'region' ) );
+      .attr( 'id', R.prop( 'abbr' ) )
+      .attr( 'class', R.prop( 'region' ) )
+      .attr( 'd', path );
+
+  labels
+    .selectAll( 'path' )
+    .data( polygonBoard )
+    .enter()
+      .append( 'text' )
+      .attr( 'id', R.compose( R.concat( 'label-' ), R.prop( 'abbr' ) ) )
+      .attr( 'x', labelX )
+      .attr( 'y', labelY )
+      .text( labelText );
 
 } );
