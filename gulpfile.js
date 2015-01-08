37d5d72b4c3d00e0d249e55d3cb049d666f5cdef
Taylor1791 <Taylor@Everding.email>
2015-01-07 20:02:16 -0600

    update FEW

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,23 @@
-var files = require( './front-end-workflow/gulpfile' );
+var few = require( './front-end-workflow/gulpfile' );
 
-files.node = [ 'gulpfile.js' ];
-files.browser = [ 'client/**/*.js' ];
-files.json = [ 'package.json', 'client/**/*.json' ];
-files.html = [ 'client/**/*.html' ];
-files.serve = [
-  'client',
-  'node_modules/d3',
-  'node_modules/less/dist',
-  'node_modules/mithril',
-  'node_modules/normalize.css',
-  'node_modules/ramda'
-];
+few.files = {
+  'package': [ __dirname + '/package.json' ],
+  node: [ 'gulpfile.js' ],
+  browser: [ 'client/**/*.js' ],
+  json: [ 'package.json', 'client/**/*.json' ],
+  html: [ 'client/**/*.html' ],
+  serve: './client',
+  unit: [
+    'node_modules/d3/d3.js',
+    'node_modules/mithril/mithril.js',
+    'node_modules/ramda/ramda.js'
+  ],
+  libraries: {
+    '/normalize.css': 'node_modules/normalize.css/normalize.css',
+    '/less.js': 'node_modules/less/dist/less.js',
+
+    '/d3.js': 'node_modules/d3/d3.js',
+    '/ramda.js': 'node_modules/ramda/ramda.js',
+    '/mithril.js': 'node_modules/mithril/mithril.js',
+  }
+};
